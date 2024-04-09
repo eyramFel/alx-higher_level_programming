f9cfeb01bb7448ab545d1a2cf98791b70dbc3985
eyramFel <fianyekufelix@live.com>
2024-04-08 18:08:09 -0700

    python_more

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,6 +3,7 @@
 This module defines the a Rectangle Object.
 """
 
+
 class Rectangle:
     """Retangle object with getter and setters
     """
@@ -10,19 +11,19 @@ class Rectangle:
         self.height = height
         self.width = width
 
-        @property
-        def width(self):
-            return self.__width
+    @property
+    def width(self):
+        return self.__width
 
-        @width.setter
-        def width(self, value):
+    @width.setter
+    def width(self, value):
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
         elif value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
 
-        @property
+    @property
     def height(self):
         return self.__height
 
